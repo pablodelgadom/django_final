570e022608c0a05ada2a2bab22f095d8928edc40
pablodelgadom <pablo00dm00@gmail.com>
2021-04-08 12:49:46 +0200

    f

--- TrabajoFinal/nucleo/models.py
+++ TrabajoFinal/nucleo/models.py
@@ -1,3 +1,46 @@
+from typing import TextIO
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Cliente(models.Model):
+    dni=models.CharField(max_length=10, null=False, blank=False)
+    nombre=models.CharField(max_length=30, null=False, blank=False)
+    apellidos=models.CharField(max_length=50, null=False, blank=False)
+    direccion=models.CharField(max_length=100, null=False, blank=False)
+    fechaNacimiento=models.DateField(verbose_name="Fecha de Nacimiento", null=False)
+    foto=models.ImageField
+    idUsuario=models.ForeignKey(User, on_delete=models.CASCADE, max_length=11, null=False)
+
+    class Meta:
+        verbose_name="Cliente"
+        verbose_name_plural="Clientes"
+
+    def __str__(self):
+        return self.nombre+" "+self.apellidos
+
+
+class Cita:
+    fecha=models.DateField(verbose_name="Fecha", null=False)
+    idCliente=models.ForeignKey(Cliente, on_delete=models.CASCADE, null=False)
+    idEspecialista=models.ForeignKey(Especialista, on_delete=models.CASCADE, null=False)
+    informe=models.TextField(null=False, blank=False)
+    realizada=models.BooleanField(null=False, blank=False)
+
+
+class Mensaje:
+    idEmisor=models.ForeignKey(User, on_delete=models.CASCADE, max_length=11, null=False)
+    idReceptor=models.ForeignKey(User, on_delete=models.CASCADE, max_length=11, null=False)
+    fecha=models.DateField(verbose_name="Fecha", null=False)
+    asunto=models.CharField(max_length=50, null=False, blank=False)
+    texto=models.TextField(null=False, blank=False)
+    leido=models.BooleanField(null=False, blank=False)
+
+
+    class Meta:
+        verbose_name="Mensaje"
+        verbose_name_plural="Mensajes"
+
+    def __str__(self):
+        return self.asunto
