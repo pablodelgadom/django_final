9a630c826830fe58a7f1cb89cd4f4e6f3d2dfda1
pablodelgadom <pablo00dm00@gmail.com>
2021-04-11 16:13:10 +0200

    modelos

--- TrabajoFinal/nucleo/models.py
+++ TrabajoFinal/nucleo/models.py
@@ -20,34 +20,7 @@ class Cliente(models.Model):
     def __str__(self):
         return self.nombre+" "+self.apellidos
 
-
-class Cita:
-    fecha=models.DateField(verbose_name="Fecha", null=False)
-    idCliente=models.ForeignKey(Cliente, on_delete=models.CASCADE, null=False)
-    idEspecialista=models.ForeignKey(Especialista, on_delete=models.CASCADE, null=False)
-    informe=models.TextField(null=False, blank=False)
-    realizada=models.BooleanField(null=False, blank=False)
-
-
-class Mensaje:
-    idEmisor=models.ForeignKey(User, on_delete=models.CASCADE, max_length=11, null=False)
-    idReceptor=models.ForeignKey(User, on_delete=models.CASCADE, max_length=11, null=False)
-    fecha=models.DateField(verbose_name="Fecha", null=False)
-    asunto=models.CharField(max_length=50, null=False, blank=False)
-    texto=models.TextField(null=False, blank=False)
-    leido=models.BooleanField(null=False, blank=False)
-
-
-    class Meta:
-        verbose_name="Mensaje"
-        verbose_name_plural="Mensajes"
-
-    def __str__(self):
-        return self.asunto
-
-
-
-class especialista:
+class Especialista(models.Model):
     dni=models.CharField(max_length=10, null=False, blank=False)
     nombre=models.CharField(max_length=30, null=False, blank=False)
     apellidos=models.CharField(max_length=50, null=False, blank=False)
@@ -63,3 +36,35 @@ class especialista:
 
     def __str__(self):
         return self.nombre+" "+self.apellidos
+
+
+class Cita(models.Model):
+    fecha=models.DateField(verbose_name="Fecha", null=False)
+    idCliente=models.ForeignKey(Cliente, on_delete=models.CASCADE, null=False)
+    idEspecialista=models.ForeignKey(Especialista, on_delete=models.CASCADE, null=False)
+    informe=models.TextField(null=False, blank=False)
+    realizada=models.BooleanField(null=False, blank=False)
+
+    class Meta:
+        verbose_name="Cita"
+        verbose_name_plural="Citas"
+
+    def __str__(self):
+        return self.fecha
+
+
+class Mensaje(models.Model):
+    idEmisor=models.ForeignKey(User, on_delete=models.CASCADE, related_name='emisor', max_length=11, null=False)
+    idReceptor=models.ForeignKey(User, on_delete=models.CASCADE, related_name='receptor', max_length=11, null=False)
+    fecha=models.DateField(verbose_name="Fecha", null=False)
+    asunto=models.CharField(max_length=50, null=False, blank=False)
+    texto=models.TextField(null=False, blank=False)
+    leido=models.BooleanField(null=False, blank=False)
+
+
+    class Meta:
+        verbose_name="Mensaje"
+        verbose_name_plural="Mensajes"
+
+    def __str__(self):
+        return self.asunto
--- TrabajoFinal/nucleo/views.py
+++ TrabajoFinal/nucleo/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+#Portada
+
+def Portada(request):
+    noticias=Noticia.objects.all()
+    data={'noticias':noticias}
+    return render(request, 'nucleo/Portada.html', data)
