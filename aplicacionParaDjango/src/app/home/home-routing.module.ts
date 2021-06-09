764bc63cc8da835451f1eedadd1adb92b83b5f80
pablodelgadom <pablo00dm00@gmail.com>
2021-06-09 23:00:35 +0200

    app

--- aplicacionParaDjango/src/app/home/home-routing.module.ts
+++ aplicacionParaDjango/src/app/home/home-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePage } from './home.page';
+import { LoginPage } from '../pages/login/login.page';
 
 const routes: Routes = [
   {
     path: '',
     component: HomePage,
-  }
+  },
+  {
+    path: 'login',
+    component: LoginPage
+    } ,
 ];
 
 @NgModule({
